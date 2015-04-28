e77d825cf6702088b41e556856a0730f39b6619c
kidrocky <kidrocky@21cn.com>
2015-04-28 19:46:08 +0800

    写标签

--- app/src/main/java/com/example/administrator/inventorytools/WriteRfidActivity.java
+++ app/src/main/java/com/example/administrator/inventorytools/WriteRfidActivity.java
@@ -3,14 +3,10 @@ package com.example.administrator.inventorytools;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class WriteRfidActivity extends ActionBarActivity
 {
-    private int connect_stat;
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -19,14 +15,14 @@ public class WriteRfidActivity extends ActionBarActivity
 
         // 从intent中获取联机状态
         Intent intent = getIntent();
-        connect_stat = intent.getIntExtra("connect_stat", 0);
+        int connect_stat = intent.getIntExtra("connect_stat", 0);
         if ( connect_stat == 0 )
         {
-            this.setTitle("脱机盘点");
+            this.setTitle("脱机写标签");
         }
         else
         {
-            this.setTitle("联机盘点");
+            this.setTitle("联机写标签");
         }
     }
 }
