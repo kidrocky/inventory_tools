24d3d0f6f2e31e194eda667927e884a9a6e8f82b
kidrocky <kidrocky@21cn.com>
2015-04-29 10:03:36 +0800

    写标签

--- app/src/main/java/com/example/administrator/inventorytools/WriteRfidActivity.java
+++ app/src/main/java/com/example/administrator/inventorytools/WriteRfidActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -12,7 +11,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.hdhe.uhf.reader.Tools;
